3132b714999e35035b9371fd2a0a258f23b0b638
Daniel Ukpele <164839562+ekojodu@users.noreply.github.com>
2025-10-05 12:26:41 +0100

    Update DifficultySelector.jsx

--- src/components/DifficultySelector.jsx
+++ src/components/DifficultySelector.jsx
@@ -4,13 +4,11 @@ const DifficultySelector = ({ difficulty, setDifficulty }) => {
 	return (
 		<div>
 			<h3>Select Difficulty</h3>
-			<select
-				value={difficulty}
-				onChange={(e) => setDifficulty(e.target.value)}
-			>
-				<option value='easy'>Easy</option>
-				<option value='hard'>Hard</option>
-			</select>
+			<select value={difficulty} onChange={(e) => setDifficulty(e.target.value)}>
+	<option value="easy">Easy</option>
+	<option value="medium">Medium</option>
+	<option value="hard">Hard</option>
+</select>
 		</div>
 	);
 };
